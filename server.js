dbe2f3331493ebac37ac521055fe0121464050f4
Otari Pkhovelishvili <opkhovelishvili@gmail.com>
2023-11-12 09:09:27 +0400

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const helmet = require("helmet");
+
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
@@ -9,12 +11,26 @@ const userRouter = require("./routes/userRoute");
 const chatRouter = require("./routes/chatRoute");
 const messageRouter = require("./routes/messageRoute");
 
+const allowedOrigins = ["https://chatapp-otari.onrender.com"]; // Replace with your production app's domain
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 require("dotenv").config();
 const url = process.env.mongoDB;
 
 //My middlewares
+app.use(helmet());
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use("/api/users", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
